445c89377c3bc70785fa030938c94e6ee6a9e2ba
310mc <crissuiteuni@gmail.com>
2023-12-21 15:17:10 +0300

    12/21/2023 15:17:09

--- js/third-party/chat/tidio.js
+++ js/third-party/chat/tidio.js
@@ -1,1 +1,1 @@
-!function(){var t=document.querySelector(".sidebar-button button");t&&t.addEventListener("click",()=>{tidioChatApi.open()})}();
+!function(){const t=document.querySelector(".sidebar-button button");t&&t.addEventListener("click",()=>{tidioChatApi.open()})}();
